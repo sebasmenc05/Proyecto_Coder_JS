276fb960153e8ee8c0cd39368bd11193ca8abdf7
SebaMen <sebastianmencia26@gmail.com>
2022-09-28 14:23:15 -0300

    agrega funciones de interes

--- js/script.js
+++ js/script.js
@@ -7,7 +7,15 @@ let precioTerrenoA = 1000000
 let precioTerrenoB = 1200000
 let precioTerrenoC = 1250000
 let montoDeEntrega = 0
-let cantidadDeCuotas = 0
+let cantidadDeCuotasMensuales = 0
+let tasaDeInteresMensual = 0.03
+let saldoACancelar
+
+function calcularInteres (){
+    let interes = saldoACancelar * tasaDeInteresMensual * cantidadDeCuotasMensuales
+    return
+}
+
 
 function montoDeCuotas (saldoACancelar, cantidadDeCuotas){
     montoDeCuotas = saldoACancelar/cantidadDeCuotas
@@ -20,7 +28,7 @@ let compra = prompt("Elija el numero del terreno que desa comprar \n1- herradura
         alert ("El Precio del Terreno elejido es " + precioTerrenoA);
         montoDeEntrega = parseInt(prompt("Ingrese en pesos el importe que desea entregar para la compra"))
         cantidadDeCuotas = parseInt(prompt("ingrese cantidad de cuotas para cancelar el terreno"));                 
-        let saldoACancelar = precioTerrenoA - montoDeEntrega;
+        saldoACancelar = precioTerrenoA - montoDeEntrega;
         montoDeCuotas (saldoACancelar, cantidadDeCuotas);
         alert ("Gracias por Elejir el terreno Herradura, el precio a cancelar es de " + saldoACancelar + " en cuotas de " + montoDeCuotas);
 
@@ -28,7 +36,7 @@ let compra = prompt("Elija el numero del terreno que desa comprar \n1- herradura
         alert ("El Precio del Terreno elejido es " + precioTerrenoB);
         montoDeEntrega = parseInt(prompt("Ingrese en pesos el importe que desea entregar para la compra"))
         cantidadDeCuotas = parseInt(prompt("ingrese cantidad de cuotas para cancelar el terreno"));                 
-        let saldoACancelar = precioTerrenoB - montoDeEntrega;
+        saldoACancelar = precioTerrenoB - montoDeEntrega;
         montoDeCuotas (saldoACancelar, cantidadDeCuotas)
         alert ("Gracias por Elejir el terreno San Marcos el precio a cancelar es de " + saldoACancelar + " en cuotas de " + montoDeCuotas);  
 
@@ -36,7 +44,7 @@ let compra = prompt("Elija el numero del terreno que desa comprar \n1- herradura
         alert ("El Precio del Terreno elejido es " + precioTerrenoC);
         montoDeEntrega = parseInt(prompt("Ingrese en pesos el importe que desea entregar para la compra"))
         cantidadDeCuotas = parseInt(prompt("ingrese cantidad de cuotas para cancelar el terreno"));                 
-        let saldoACancelar = precioTerrenoC - montoDeEntrega;
+        saldoACancelar = precioTerrenoC - montoDeEntrega;
         montoDeCuotas (saldoACancelar, cantidadDeCuotas)
         alert ("Gracias por Elejir el terreno Resguardo el precio a cancelar es de " + saldoACancelar + " en cuotas de " + montoDeCuotas);       
     } else {
